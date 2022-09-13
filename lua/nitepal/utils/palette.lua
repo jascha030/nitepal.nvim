f0612f1bfc1c756b317efb107daccaf19ca30f3d
Jascha030 <contact@jaschavanaalst.nl>
2022-09-13 12:05:59 +0200

    🎨 Cleanup palette.lua

--- lua/nitepal/utils/palette.lua
+++ lua/nitepal/utils/palette.lua
@@ -1,53 +1,58 @@
 local M = {}
 
-local variants = {
-    light = {
-        bg = '#e7e9ef',
-        fg = '#444a73',
+local colors = {
+    variants = {
+        light = {
+            bg =         '#e7e9ef',
+            fg =         '#444a73',
+        },
+        dark = {
+            bg =         '#1e2030',
+            fg =         '#c8d3f5',
+        },
     },
-    dark = {
-        bg = '#1e2030',
-        fg = '#c8d3f5',
-    },
-}
+    palette = {
+        white =          '#969ac9',
+        bright_white =   '#fffcfc',
+        alt_white =      '#6183bb',
 
-local palette = {
-    black = '#32466e',
-    red = '#ea1479',
-    green = '#2da44e',
-    yellow = '#ffcc00',
-    blue = '#3d59a1',
-    magenta = '#6f42c1',
-    cyan = '#0a6e81',
-    white = '#969ac9',
+        black =          '#32466e',
+        bright_black =   '#465a82',
+        alt_black =      '#1D202F',
 
-    orange = '#f05033',
-    purple = '#9d7cd8',
+        red =            '#ea1479',
+        bright_red =     '#f47cb4',
+        dark_red =       '#d4007c',
+        alt_red =        '#ff007c',
 
-    bright_black = '#465a82',
-    bright_red = '#f47cb4',
-    bright_green = '#9ece6a',
-    bright_yellow = '#ffae31',
-    bright_blue = '#8494FF',
-    bright_magenta = '#bb9af7',
-    bright_cyan = '#1abc9c',
-    bright_white = '#fffcfc',
-    bright_orange = '#ff8000',
+        orange =         '#f05033',
+        bright_orange =  '#ff8000',
 
-    alt_black = '#1D202F',
-    alt_red = '#ff007c',
-    alt_green = '#8fd742',
-    alt_blue = '#0083f7',
+        yellow =         '#ffcc00',
+        bright_yellow =  '#ffae31',
 
-    alt_magenta = '#5f5fec',
-    alt_cyan = '#41a6b5',
-    alt_white = '#6183bb',
+        green =          '#2da44e',
+        bright_green =   '#9ece6a',
+        dark_green =     '#5d7b23',
+        alt_green =      '#8fd742',
 
-    dark_blue = '#364A82',
-    dark_green = '#5d7b23',
-    dark_red = '#d4007c',
+        cyan =           '#0a6e81',
+        bright_cyan =    '#1abc9c',
+        alt_cyan =       '#41a6b5',
 
-    none = 'NONE',
+        blue =           '#3d59a1',
+        bright_blue =    '#8494FF',
+        dark_blue =      '#364A82',
+        alt_blue =       '#0083f7',
+
+        magenta =        '#6f42c1',
+        bright_magenta = '#bb9af7',
+        alt_magenta =    '#5f5fec',
+
+        purple =         '#9d7cd8',
+
+        none =           'NONE',
+    }
 }
 
 local function merge_colors(types, shared)
@@ -65,7 +70,7 @@ local function merge_colors(types, shared)
 end
 
 function M.get_colors()
-    return merge_colors(variants, palette)
+    return merge_colors(colors.variants, colors.palette)
 end
 
 return M
