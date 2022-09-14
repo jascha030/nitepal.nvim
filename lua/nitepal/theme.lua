a32436c0200b41dacebea1cdcc7bbfa7d6a17989
Jascha030 <contact@jaschavanaalst.nl>
2022-09-14 14:15:36 +0200

    ♻️ Extract method to generate lualine scheme like normal scheme.

--- lua/nitepal/theme.lua
+++ lua/nitepal/theme.lua
@@ -206,4 +206,37 @@ function M.init()
     -- util.autocmds(theme.config)
 end
 
+function M.generate_lualine(style)
+    local _, scheme = require('nitepal.theme').generate(style)
+
+    return {
+        normal = {
+            a = { bg = scheme.blue1, fg = scheme.black },
+            b = { bg = scheme.fg_gutter, fg = scheme.blue1 },
+            c = { bg = scheme.bg_statusline, fg = scheme.fg_sidebar },
+        },
+        insert = {
+            a = { bg = scheme.green, fg = scheme.black },
+            b = { bg = scheme.fg_gutter, fg = scheme.green },
+        },
+        command = {
+            a = { bg = scheme.yellow, fg = scheme.black },
+            b = { bg = scheme.fg_gutter, fg = scheme.yellow },
+        },
+        visual = {
+            a = { bg = scheme.magenta, fg = scheme.black },
+            b = { bg = scheme.fg_gutter, fg = scheme.magenta },
+        },
+        replace = {
+            a = { bg = scheme.red, fg = scheme.black },
+            b = { bg = scheme.fg_gutter, fg = scheme.red },
+        },
+        inactive = {
+            a = { bg = scheme.bg_statusline, fg = scheme.blue },
+            b = { bg = scheme.bg_statusline, fg = scheme.fg_gutter, gui = 'bold' },
+            c = { bg = scheme.bg_statusline, fg = scheme.fg_gutter },
+        },
+    }
+end
+
 return M
