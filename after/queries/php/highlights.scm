209f21379eee766cef683a0c9d39b4c970cf94f1
Jascha030 <contact@jaschavanaalst.nl>
2022-10-14 10:34:08 +0200

    🔧 Tweak php highlights.scm

--- after/queries/php/highlights.scm
+++ after/queries/php/highlights.scm
@@ -1,27 +1,20 @@
-((class_declaration 
-   name: (name) @className)
- (set! "priority" 105))
+[
+  "use"
+  "namespace"
+  "static"
+  "final"
+] @keyword.scope
 
-((trait_declaration 
-   name: (name) @className)
- (set! "priority" 105))
-
-((class_declaration 
-   modifier: (final_modifier) @modifier.final)
- (set! "priority" 105))
-
-((scoped_call_expression
-   scope: (relative_scope) @scope.relative)
- (set! "priority" 105))
-
-((member_call_expression
-   object: (variable_name) @object.var)
- (set! "priority" 105))
-
-((class_constant_access_expression
-  . [(name) (qualified_name)] @className
-  (name) @constant)
-   (set! "priority" 105))
-
-((namespace_use_declaration) @namespace.use
- (set! "priority" 105))
+[
+  (class_declaration 
+    (name) @class.declaration)
+  (trait_declaration 
+    name: (name) @class.declaration)
+  (scoped_call_expression 
+    scope: (relative_scope) @scope.relative)
+  (member_call_expression 
+    object: (variable_name) @object.var)
+  (class_constant_access_expression . 
+                                    [(name) (qualified_name)] @class.name
+                                    (name) @constant)
+] (set! "priority" 105)
