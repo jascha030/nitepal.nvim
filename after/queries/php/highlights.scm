1ff5cff869a954007963aff62f62bb832ca61461
Jascha030 <contact@jaschavanaalst.nl>
2022-10-17 22:53:03 +0200

    ✨ Add a bunch of highlights for php.

--- after/queries/php/highlights.scm
+++ after/queries/php/highlights.scm
@@ -1,22 +1,34 @@
 ; extends
 
 [
+  (class_declaration (name) @class.declaration)
+  (trait_declaration name: (name) @class.declaration)
+  (class_constant_access_expression 
+    (qualified_name 
+      (namespace_name_as_prefix) @reference.scope))
+  (scoped_call_expression scope: (relative_scope) @scope.relative)
+  (scoped_call_expression scope: (name) @class.declaration)
+  (member_call_expression object: (variable_name) @object.var)
+  (class_constant_access_expression 
+    . 
+    [(name) (qualified_name)] @class.declaration
+    (name) @constant.class)
+] (set! "priority" 105)
+
+[
   "use"
   "namespace"
   "static"
   "final"
 ] @keyword.scope
 
-[
-  (class_declaration 
-    (name) @class.declaration)
-  (trait_declaration 
-    name: (name) @class.declaration)
-  (scoped_call_expression 
-    scope: (relative_scope) @scope.relative)
-  (member_call_expression 
-    object: (variable_name) @object.var)
-  (class_constant_access_expression . 
-                                    [(name) (qualified_name)] @class.declaration
-                                    (name) @constant)
-] (set! "priority" 105)
+(variable_name 
+  ("$") @variable.operator)
+
+(simple_parameter 
+  name: (variable_name 
+          ("$") @parameter.operator))
+
+(object_creation_expression 
+  (name) @object.var)
+
