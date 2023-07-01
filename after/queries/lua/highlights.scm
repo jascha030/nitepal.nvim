465539592dbd48a0dec480525846acb03ecccbb7
Jascha030 <contact@jaschavanaalst.nl>
2023-07-01 04:28:12 +0200

    🎨 Add highlight queries

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -1,10 +1,18 @@
+; extends
+
+; Function names
+
 [
-  (if_statement
-  [
-   "if"
-   "else"
-   "elseif"
-   "then"
-   "end"
-   ] @conditional.keyword)
+  (if_statement 
+    [
+      "if"
+      "else"
+      "elseif"
+      "then"
+      "end"
+    ] @conditional.keyword)
 ] (set! "priority" 105)
+
+(function_declaration name: 
+                        (identifier) @function)
+
