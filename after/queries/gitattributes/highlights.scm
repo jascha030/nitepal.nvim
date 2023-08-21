d0e2e209054d17c85558f48bb48c619609d5063f
Jascha030 <contact@jaschavanaalst.nl>
2023-08-21 23:14:03 +0200

    🌐 Add highlight captures for gitattributes lang.

---
+++ after/queries/gitattributes/highlights.scm
@@ -0,0 +1,12 @@
+; extends
+
+(builtin_attr) @keyword
+(attr_set) @operator
+(trailing_slash) @punctuation.delimiter
+(wildcard) @constant
+ 
+(pattern [
+          absolute: (dir_sep) 
+          relative: (dir_sep)
+          ] @punctuation.delimiter) @variable
+
