686a2f95e4526f709301debea701e00de4fb669e
Jascha030 <contact@jaschavanaalst.nl>
2023-12-13 10:11:04 +0100

    🩹 Minor query fixes

--- after/queries/php/highlights.scm
+++ after/queries/php/highlights.scm
@@ -1,36 +1,35 @@
-; extends
+;; extends
 
-[
+([
   "use"
   "namespace"
-  "static"
+  "static" 
   "final"
-] @keyword.scope
+  ] @keyword.scope)
 
-[
+([
   "try"
   "catch"
   "throw"
   "instanceof"
   "from"
   "as"
-] @keyword
+ ] @keyword)
 
-(binary_expression 
+(binary_expression
   [
-    "??"
-    "?"
-  ] @conditional.operator)
+   "??"
+   "?"
+   ] @conditional.operator)
 
 (optional_type ["?"] @operator)
 (arguments (argument name: (name) @parameter.name))
-
 (declare_statement 
-  (declare_directive (integer) @number
-                     ) @variable)
+  (declare_directive 
+    (integer) @number) 
+  @variable)
 
-
-[
+([
   (class_declaration 
     (name) @class.declaration)
   (trait_declaration 
@@ -46,23 +45,27 @@
     object: (variable_name) @object.var)
   (class_constant_access_expression 
     . 
-    [(name) (qualified_name)] @class.declaration
+    [(name) (qualified_name)] @class.declaration 
     (name) @constant.class)
   (interface_declaration 
     name: (name) @interface.declaration)
-  (method_declaration (visibility_modifier) @scope.relative)
-  (property_declaration (visibility_modifier) @keyword)
-] (set! "priority" 105)
+  (method_declaration 
+    (visibility_modifier) @scope.relative)
+  (property_declaration 
+    (visibility_modifier) @keyword)
+  ] 
+ (#set! "priority" 105))
 
-((name) @keyword (#match? @keyword "^self$"))
+((name) @keyword 
+        (#match? @keyword "^self$"))
 ((name) @keyword (#match? @keyword "^(exit|die)$"))
 
 (variable_name ("$") @variable.operator)
 (simple_parameter 
   name: (variable_name 
           ("$") @parameter.operator
-          (name) @parameter) 
-  (set! "priority" 105))
+          (name) @parameter)
+  (#set! "priority" 105))
 
 (object_creation_expression (name) @object.var)
 (variadic_parameter name: (variable_name) @parameter) @variable
