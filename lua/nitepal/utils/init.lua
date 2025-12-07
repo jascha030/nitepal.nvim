212e3902a4544a0b292060f79921a55ed64e61e0
Jascha030 <contact@jaschavanaalst.nl>
2025-12-07 01:29:36 +0100

    ♻️ Tweak color methods.

--- lua/nitepal/utils/init.lua
+++ lua/nitepal/utils/init.lua
@@ -3,6 +3,8 @@ local hsluv = require('nitepal.utils.hsluv')
 ---@class nitepal.Utils
 local M = {}
 
+M.day_brightness = 0.8
+
 local function darken(n, f)
     return n * (1 - f)
 end
@@ -43,6 +45,24 @@ function M.brighten(hex, perc)
     hsl[3] = hsl[3] + lSpace * perc
 
     return hsluv.hsluv_to_hex(hsl)
+end
+
+function M.lightmode(color)
+    if type(color) == 'table' then
+        for key, value in pairs(color) do
+            color[key] = M.lightmode(value)
+        end
+    elseif type(color) == 'string' then
+        if color ~= 'NONE' then
+            local hsl = hsluv.hex_to_hsluv(color)
+
+            hsl[3] = hsl[3] > 70 and hsl[3] - 5 or hsl[3]
+            hsl[2] = hsl[2] < 40 and hsl[2] + 3 or hsl[2]
+
+            return hsluv.hsluv_to_hex(hsl)
+        end
+    end
+    return color
 end
 
 return M
